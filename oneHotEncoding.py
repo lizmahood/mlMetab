60b7f2a7d28dd50107611f6071e96e549ba474a7
lizmahood <lizmahood@yahoo.com>
2020-04-26 23:16:07 -0400

    Made multiclass, now accepts variable binwidths, makes test and training arff, python3

--- oneHotEncoding.py
+++ oneHotEncoding.py
@@ -1,100 +1,151 @@
-import sys
+import sys, numpy as np, random
+from collections import Counter
 
-print("INP1: Input file")
-print("INP2: Index for class")
-print("INP3: Name of positive class for binary classification")
-file1=open(sys.argv[1],'r')
-index=int(sys.argv[2])
-cname=sys.argv[3]
-line1=file1.readline()
-
-#Make ARFF file
-out1=open(sys.argv[1]+".%s.arff"%cname,'w')
-out1x=open(sys.argv[1]+".%s.arff.instanceIDs"%cname,'w')
-biglist=range(50,2500,1)
-out1.write('@relation\tMSMS_binary_class\n\n')
-for item in biglist:
-    out1.write('@attribute\t%s\tnumeric\n'%item)
-out1.write('@attribute\tclass\t{%s,not_%s}\n\n'%(cname,cname))
-out1.write('@data\n')
-pos=0; neg=0; m=0
-while line1:
-    if line1.startswith('#'):
-        pass
-    else:
-        tab1=line1.strip().split('\t')
-        
-        em=int(float(tab1[7]))
-        #print tab1, em
-        #Get the class name
-        cat=tab1[index]
-        if cat==cname:
-          ncat=cname
-          pos+=1
+def argparsr(argl):
+    ##argl is the list of arguments the user inputted
+    for ar in range(1, len(argl)):
+        print(ar)
+        if argl[ar] == '-inp':
+            inpf = argl[ar + 1]
+        elif argl[ar] == '-binw':
+            binw = argl[ar + 1]
+        elif argl[ar] == '-output':
+            outp = argl[ar + 1]
+        elif argl[ar] == '-perct':
+            perc = argl[ar + 1]
         else:
-          ncat=('not_%s'%cname)
-          neg+=1
+            pass
+    return(inpf, binw, outp, perc)
 
-        #Get peaks
-        peaks=tab1[8].split(',')
-        plist=sorted([int(float(peak)) for peak in peaks])
-        plist.append(em)        
+def show_help():
+    print('ARGS:\n'\
+        '-inp full path to input file\n'\
+        '-outp full path to desired output file\n'\
+        '-perct what percentage (as decimal) do you want for the test set?\n'\
+        '-binw what is your preferred binwidth?')
 
-        #One hot encoding
-        encoded=[]
-        for item in biglist:
-            if item in plist:
-                encoded.append('1')
+def parse_mgf(inp):
+
+    ##inp is a file object of the input mgf file
+    ##RETURNS: Dict, with class as key and name,
+    ##m/z, and MS/MS peaks as values.
+    mgfd = {}
+    strt = True
+    for inpl in inp:
+        if inpl.startswith('Name') and strt == True:
+            nam = inpl[6:].strip()
+            clas = nam.split()[0].replace('[','').replace(']','')
+            name = nam.replace(' ', '_').replace(':', '-').replace(';', '').replace('}','')
+            mgfd[clas] = []
+            strt = False
+        elif inpl.startswith('Name') and strt != True:
+            if clas in mgfd.keys():
+                mgfd[clas].append([name, clas, mz, peaks])
             else:
-                encoded.append('0')
-        hot=','.join(encoded)
-        out1.write('%s,%s\n'%(hot,ncat))
-        out1x.write('%s\t%s\n'%(m,tab1[0]))
+                mgfd[clas] = [[name, clas, mz, peaks]]
 
+            ##resetting variables for next entry
+            nam = inpl[6:].strip()
+            clas = nam.split()[0].replace('[','').replace(']','')
+            name = nam.replace(' ', '_').replace(':', '-').replace(';','').replace('}','')
+        elif inpl.startswith('MW:'):
+            mz = float(inpl.strip().split(': ')[1])
+        elif inpl.startswith('Num Peaks:'):
+            peaks = []
+            inpl =inp.readline()
+            while inpl[:1].isdigit():
+                ##we are just logging mz, not abund
+                peaks.append(float(inpl.strip().split()[0]))
+                inpl = inp.readline()
 
-        m+=1
-        if m%1000==0:
-            print m, pos, neg
-          
-        
-    line1=file1.readline()
-file1.close(); out1.close(); out1x.close()
+    mgfd[clas].append([name, clas, mz, peaks])
+    return(mgfd)
 
-file1=open(sys.argv[1]+".%s.arff"%cname,'r')
-out1=open(sys.argv[1]+".%s.arff.TEST.arff"%cname,'w')
-out2=open(sys.argv[1]+".%s.arff.MAIN.arff"%cname,'w')
-line1=file1.readline()
-m=0; n=0
-while line1:
-    if line1.startswith('#') or line1.startswith('@') or line1=='':
-        out1.write(line1)
-        out2.write(line1)
-    elif line1.strip().split()==[]:
-        out1.write(line1)
-        out2.write(line1)
-    else:
-        sp=line1.strip().split(',')
-        if sp[-1]==cname:
-            if m<20:                
-                out1.write('%s,not_%s\n'%(','.join(sp[0:-1]),cname))
-                m+=1
-            else:
-                out2.write(line1)
+def onehot(lin, bigl):
+    ##lin is a list with a sublist of MS/MS peaks
+    ##bigl is the list of ranges for encoding
+    ##RETURNS: lin with the sublist of peaks
+    ##one-hot encoded.
+    peaks = lin[3]
+    ohpeaks = []
+    for ran in range(len(bigl)-1):
+        st = float(bigl[ran])
+        end = float(bigl[ran + 1])
+        if (any(peak >= st and peak <= end for peak in peaks)):
+            ohpeaks.append(1)
+        else: ohpeaks.append(0)
+
+    lin[3] = ohpeaks
+    return lin
+
+def make_arff(bnw, mgfd, out, p):
+    ##mgfd is the dict containing output of parse_mgf
+    ##binwidth is float or int
+    ##OUTPUTS: arff file with peaks as
+    ##one-hot encoded features
+
+    #Initialize ARFF file
+    train=open(out + 'training.arff','w')
+    test=open(out + 'test.arff','w')
+
+    biglist=np.arange(50,2500,float(bnw))
+    train.write('@relation\tMSMS_multiclass\n\n')
+    test.write('@relation\tMSMS_multiclass\n\n')
+
+    train.write('@attribute\tID\tstring\n')
+    test.write('@attribute\tID\tstring\n')
+    
+    for item in biglist:
+        train.write(f'@attribute\t{item}\tnumeric\n')
+        test.write(f'@attribute\t{item}\tnumeric\n')
+
+    ##adding line for all possible classes
+    clssls = mgfd.keys()
+
+    clsstr = ','.join(clssls)
+    print(clsstr)
+    train.write(f'@attribute\tclass\t{{{clsstr}}}\n\n')
+    test.write(f'@attribute\tclass\t{{{clsstr}}}\n\n')
+    train.write('@data\n')
+    test.write('@data\n')
+
+    ##reserving instances for the test arff
+    rlst = []
+    trlst = []
+    for insl in mgfd.values():
+        if len(insl) >= 10:
+            sampnum = int(float(p) * len(insl))
         else:
-            if n<20:                
-                out1.write('%s,%s\n'%(','.join(sp[0:-1]),cname))
-                n+=1
-            else:
-                out2.write(line1)
-    line1=file1.readline()
-file1.close(); out1.close(); out2.close()
-            
+            sampnum = 1
+        tmp = random.sample(insl, sampnum)
 
-print "Positive: ", pos
-print "Negative: ", neg
-print "Total out: ", m
-print "Done!"
-          
-          
+        ##now getting remainder
+        remainder = insl
+        for x in tmp:
+            remainder.remove(x)
+
+        ##getting instances not in tmp (these are the training ones)
+        rlst.append(tmp)
+        trlst.append(remainder)
         
+    flatr = [item for sublist in rlst for item in sublist]
+    trainr = [item for sublist in trlst for item in sublist]
+    print('done with making lists!')
+    ##doing one-hot now and writing out
+    ##first for test arff instances
+    for ind in range(len(flatr)):
+        flatr[ind] = onehot(flatr[ind], biglist)
+        featstr = ','.join(str(x) for x in flatr[ind][3])
+        test.write(f'{flatr[ind][0]},{featstr},{flatr[ind][1]}\n')
 
+    ##now training
+    for ind in range(len(trainr)):
+        trainr[ind] = onehot(trainr[ind], biglist)
+        featstr = ','.join(str(x) for x in trainr[ind][3])
+        train.write(f'{trainr[ind][0]},{featstr},{trainr[ind][1]}\n')
+    
+
+
+infil, binw, outf, per = argparsr(sys.argv)
+mgffd = parse_mgf(open(infil, 'r'))
+make_arff(binw, mgffd, outf, per)
